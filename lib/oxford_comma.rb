4e23905d1f75816f8457caba57dd9983c2d58f6c
Otto Cedeno <otto@ottostacos.com>
2019-11-07 22:49:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,16 @@ def oxford_comma(array)
     return array.join(", ")
   end
 end
+
+
+#solution from Flatiron
+#I like how they solved with only two conditionals
+
+# def oxford_comma(array)
+#   if array.length == 2
+#     return "#{array[0]} and #{array[1]}"
+#   elsif 2 < array.length
+#     array[-1].insert(0, "and ")
+#   end
+#   array.join(", ")
+# end
