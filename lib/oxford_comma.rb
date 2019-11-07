0332a073e5afd2058dce29a4300780b5c72e06fc
Otto Cedeno <otto@ottostacos.com>
2019-11-07 22:37:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    return array[array.size - 1].to_s
+  elsif array.size == 2
+    return array.join(' and ')
+  else
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  end
+  # binding.pry
 end
+
+four = ["Otto", "has", "a", "dog"]
+single = ["Otto"]
+
+puts oxford_comma(four)
